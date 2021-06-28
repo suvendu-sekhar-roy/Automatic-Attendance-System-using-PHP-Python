a4b5311024c03a85feb78ab2fb5ce64ceb749644
suvendu-sekhar-roy <68660993+suvendu-sekhar-roy@users.noreply.github.com>
2021-06-28 08:27:02 +0530

    Update teachers view.php

--- view/teachers view.php
+++ view/teachers view.php
@@ -1,29 +1,33 @@
 
 <!DOCTYPE html>
 <html lang="en">
+    <?php
+    session_start();
+    ?>
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Teachers Home</title>
   <link rel="stylesheet" href="../assets/style.css">
+    <link rel="stylesheet" href="../assets/css/style.css" />
   
 </head>
 <body style="background-color: rgb(207, 196, 255);;">
     <img src="../assets/Images/logo.png" class="logo" />
     <div id="wrapper">
         <div id="header">
-            <h1>All Teachers</h1>
+            <h1>Check Attendance here</h1>
         </div>
         <div id="menu">
             <ul>
-                
+                <!--
                 <li>
                     <a href="../view/attendance.php">Check Attendance</a>
              
-                </li>
+                </li>-->
                 <li>
-                    <a href="../videoStream.html">on camera</a>
+                    <a href="../logout.php">Log Out</a>
              
                 </li>
             </ul>
@@ -31,57 +35,116 @@
 	</div>
 
 
-<div id="main-content">
-    <h2>All Records</h2>
+    <center><h1>Welcome <?php echo $_SESSION['name']?></h1></center>
+    
+    <div id="main-content">
+    <h2>Show Records using Roll</h2>
+    <form class="post-form" action="<?php $_SERVER['PHP_SELF'];?>" method="post">
+        <div class="form-group">
+            <label>Roll</label>
+            <input type="text" name="sid" />
+        </div>
+        <input class="submit" type="submit" name="showbtn" />
+    </form>
 
-    <?php 
-        //Connection
-        $conn= mysqli_connect("localhost", "root","", "attendance") or die("Connection failed");
+<!--Show Records using Roll-->
+    <?php
+        if(isset($_POST['showbtn'])){
+            //Connection
+    $conn= mysqli_connect("localhost", "root","", "attendance") or die("Connection failed");
 
-        //Run SQL quary
-        $sql= "select * from teacher";
-        $result= mysqli_query($conn, $sql) or die("Query unseccesful"); 
-        if (mysqli_num_rows($result) > 0) {
-    ?>
-        
-    <table cellpadding="7px">
-        <thead>
-            <th>Id</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Phone</th>
-            <th>Depertment</th>
-            <!--<th>Action</th>-->
-        </thead>
-        <tbody>
-        <?php
-            while($row = mysqli_fetch_assoc($result)){
-
+    //Run SQL quary
+    $stu_id=$_POST['sid'];
+    $sql= "select * from student inner join record on student.Roll= record.Roll where record.Roll={$stu_id}";
+    $results= mysqli_query($conn, $sql) or die("Query unseccesful"); 
+    if (mysqli_num_rows($results) > 0) { ?>
+        <!--if ($row=mysqli_fetch_assoc($results)){
         ?>
-            <tr>
-                <td><?php echo $row['id']; ?></td>
-                <td><?php echo $row['name'];?></td>
-                <td><?php echo $row['email'];?></td>
-                <td><?php echo $row['phone'];?></td>
-                <td><?php echo $row['dept'];?></td>
-                <!--
-                <td>
-                    <a href='edit.php?id=<?php echo $row['id'];?>'>Edit</a>
-                    <a href='delete-inline.php?id=<?php echo $row['id'];?>'>Delete</a>
-                </td>
-            -->
-            </tr>
-            <?php } ?>
-        </tbody>
-    </table>
-    <?php }
-    else{
-        echo "<h2> No record Found</h2>";
-    }
-    //Close sql connection
-    mysqli_close($conn);
-    ?>
-</div>
+            <center><h3>Name: <?php //echo $row['Name']; }?></h3></center>-->
+            <table cellpadding="7px">
+                <thead>
+                    <th>Name</th>
+                    <th>Date</th>
+                    <th>Time</th>
+                </thead>
+                <tbody>
+                    <?php while ($row=mysqli_fetch_assoc($results)){  ?>
+                    
+                    <tr>
+                        <td><?php echo $row['Name']; ?></td>
+                        <td><?php echo $row['Date']; ?></td>
+                        <td><?php echo $row['Time']; ?></td>
+                    </tr>
+
+                    <?php } ?>
+                </tbody>
+            </table>
+        
+        <?php
+            }
+            else{
+                echo "<center><h2>No Record Found</h2></center>";
+            }
+            mysqli_close($conn);
+            }?>
+    
+    <!--Show Records using Date-->
+
+    <h2>Show Records using Date</h2>
+    <form class="post-form" action="<?php $_SERVER['PHP_SELF'];?>" method="post">
+        <div class="form-group">
+            <label>start Date</label>
+            <input type="text" name="date1" placeholder= 'yyyyy-mm-dd'/>
+            <label>End Date</label>
+            <input type="text" name="date2" placeholder= 'yyyyy-mm-dd'/>
+           
+        </div>
+        <input class="submit" type="submit" name="dateshow" />
+    </form>
+
+    <?php
+        if(isset($_POST['dateshow'])){
+            //Connection
+    $conn= mysqli_connect("localhost", "root","", "attendance") or die("Connection failed");
+
+    //Run SQL quary
+    $start_date=$_POST['date1'];
+    $end_date=$_POST['date2'];
+    
+    $sql= "select * from student inner join record on student.Roll= record.Roll where record.Date between '{$start_date}' and '{$end_date}'";
+    $result= mysqli_query($conn, $sql) or die("Date Query unseccesful"); 
+    
+
+    if (mysqli_num_rows($result) > 0) { ?>
+        <table cellpadding="7px">
+            <thead>
+                <th>Date</th>
+                <th>Roll</th>
+                <th>Name</th>
+                <th>Time</th>
+                
+            </thead>
+            <tbody>
+                <?php
+                    while ($row=mysqli_fetch_assoc($result)){
+                ?>
+                <tr>
+                    <td> <?php echo $row['Date']; ?></td>
+                    <td><?php echo $row['Roll']; ?></td>
+                    <td> <?php echo $row['Name']; ?></td>  
+                    <td><?php echo $row['Time'];?></td>
+                </tr>
+
+                <?php } ?>
+            </tbody>
+        </table>
+        <?php
+            }
+            else{
+                echo "<h2>No Record Found</h2>";
+            }
+            mysqli_close($conn);
+            }?>
 </div>
 </body>
 </html>
